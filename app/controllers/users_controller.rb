1c57bc1183b73535df2ddda6512b1949eb98de7a
Lena Hou <lenahou2013@gmail.com>
2021-06-29 18:31:39 -0400

    Update all the css styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,11 @@ class UsersController < ApplicationController
     end
      
     get '/users/:slug' do 
+      if !logged_in?
+        flash[:message] = "Please login first."
+        redirect "/login"
+      end
+      
         @user = User.find_by_slug(params[:slug])
         erb :"/users/show"
     end
