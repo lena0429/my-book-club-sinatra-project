2efa7010096520b321f036acd3acc6729d887efd
Lena Hou <lenahou2013@gmail.com>
2021-06-30 17:56:22 -0400

    error fixed

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 ENV['SINATRA_ENV'] ||= "development"
 
-ENV['SINATRA_ENV'] ||= "development"
-
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 Dotenv.load if ENV['SINATRA_ENV'] == "development"
